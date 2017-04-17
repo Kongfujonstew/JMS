732d935af38db7c0168e9ac8f8f33ea366f37022
Kongfujonstew <jonstew@gmail.com>
2017-04-16 19:12:09 -0700

    add colors, logo and consFunc

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import {render} from 'react-dom';
 
+
+
 class App extends React.Component {
   render () {
     return (
     <div id="main">
-      <p> Hello React!</p>
+      <div id="flexContainer" className="flexContainer">
+        <div id="blue" className="flexbox"></div>
+        <div id="red" className="flexbox"></div>
+      </div>
+      <div id="name">Jon Michael Stewart</div>
     </div>
     )
   }
